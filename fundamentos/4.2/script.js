2e622984e4ddedaa8f6660bb51f7597d6067e668
MutuBengui <mutubengui@gmail.com>
2021-11-09 15:41:09 -0400

    Criando repositórios para exercícios do dia

---
+++ fundamentos/4.2/script.js
@@ -0,0 +1,13 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices=menu[1] ;
+
+console.log(menuServices);
+
+menu.push('Contato');
+
+console.log(menu);
+
+let word = 'Hello1';
+for (let letter of word) {
+  console.log(letter);
+}
